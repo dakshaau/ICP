0271efae65433b3dee5583d8c6b3e9f2e5dc1e78
Daksh Gupta <dakshkumargupta@gmail.com>
2017-04-19 21:29:47 -0500

    Updated trial.py

--- trial.py
+++ trial.py
@@ -26,11 +26,11 @@ if __name__ == '__main__':
 	# d1 = distance(pointcloud1[0,:3], pointcloud1[:,:3])
 	# d2 = distance(pointcloud2[0,:3], pointcloud2[:,:3])
 
-	com_p1 = np.mean(pointcloud1[:,:3],axis=0).reshape((3,1))
-	com_p2 = np.mean(pointcloud2[:,:3],axis=0).reshape((3,1))
+	u_p1 = np.mean(pointcloud1[:,:3],axis=0).reshape((3,1))
+	u_p2 = np.mean(pointcloud2[:,:3],axis=0).reshape((3,1))
 
 	# print(com_p1.shape, com_p2.T.shape)
-	com_p1p2 = com_p1.dot(com_p2.T)
+	u_p1p2 = u_p1.dot(u_p2.T)
 	points1 = pointcloud1[:,:3].reshape((pointcloud1.shape[0],3,1))
 	# print(pointcloud1[:10])
 	# print(points1[:10])
@@ -41,7 +41,7 @@ if __name__ == '__main__':
 		p1p2[i,:,:] = points1[i].dot(points2[i].T)[:,:]
 
 	# print(p1p2[:10])
-	sigma_p1p2 = np.mean(p1p2, axis=0) - com_p1p2
+	sigma_p1p2 = np.mean(p1p2, axis=0) - u_p1p2
 	del p1p2
 	del points1
 	del points2
@@ -56,7 +56,7 @@ if __name__ == '__main__':
 	Q_sigma_p1p2[0,0] = np.trace(sigma_p1p2)
 	Q_sigma_p1p2[1:,0] = delta[:,0]
 	Q_sigma_p1p2[0,1:] = delta.T[0,:]
-	temp = sigma_p1p2 + sigma_p1p2.T - np.trace(sigma_p1p2)*np.eye(3)
+	temp = sigma_p1p2 + sigma_p1p2.T - (np.trace(sigma_p1p2)*np.eye(3))
 	Q_sigma_p1p2[1:,1:] = temp[:,:]
 
 	u,s,v = np.linalg.svd(Q_sigma_p1p2)
@@ -86,16 +86,18 @@ if __name__ == '__main__':
 	R[1,2] = 2*(q_r[2]*q_r[3] - q_r[0]*q_r[1])
 	R[2,1] = 2*(q_r[2]*q_r[3] + q_r[0]*q_r[1])
 
-	q_t = com_p2 - R.dot(com_p1)
+	q_t = u_p2 - R.dot(u_p1)
 
 	# q = np.hstack((R,q_t))
 	# print(R)
 	# print(q_t)
+	# q_t[0,0] += 0.0000017
 	# print(q)
 
 	rotated_p1 = R.dot(pointcloud1[:,:3].T)
+	# rotated_p1 = pointcloud1[:,:3].T
 	# print(rotated_p1.T[:5])
-	final_p1 = rotated_p1 - q_t*np.ones(rotated_p1.shape)
+	final_p1 = rotated_p1 + q_t*np.ones(rotated_p1.shape)
 	# print(q_t*np.ones(rotated_p1.shape)[:,:5])
 	# print(final_p1.shape)
 	final_p1 = final_p1.T
@@ -116,17 +118,19 @@ if __name__ == '__main__':
 	Y = pointcloud2[:10000,1]
 	Z = pointcloud2[:10000,2]
 
+	# ax.plot_wireframe(X,Y,Z, color='r')
 	ax.scatter(X,Y,Z, color='r', marker='o')
 
 	ax.set_xlabel('Latitude')
 	ax.set_ylabel('Longitude')
 	ax.set_zlabel('Altitude')
 	
-	# X = final_p1[:100,0]
-	# Y = final_p1[:100,1]
-	# Z = final_p1[:100,2]
+	X = final_p1[:10000,0]
+	Y = final_p1[:10000,1]
+	Z = final_p1[:10000,2]
 
-	# ax.plot_wireframe(X,Y,Z,rstride = 10, cstride=10, color='g')
+	# ax.plot_wireframe(X,Y,Z, color='g')
+	ax.scatter(X,Y,Z, color='g', marker='^')
 
 	plt.show()
 
