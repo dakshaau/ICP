2cd713502aae5a6db2a7a02e9758694db4b56c52
Daksh Gupta <dakshkumargupta@gmail.com>
2017-04-24 19:59:14 -0500

    Changed coordinate unit from degrees to meters

--- trial.py
+++ trial.py
@@ -63,78 +63,7 @@ def Q(p, x):
 	X[:3,:3] = R[:,:]
 	X[:3,3] = t[:,0]
 	R = X[:,:]
-	# p1p2 = np.zeros((points1.shape[0],3,3),dtype=points1.dtype)
-	# for i in range(points1.shape[0]):
-	# 	p1p2[i,:,:] = points1[i].dot(points2[i].T)[:,:]
-
-	# # print(p1p2[:10])
-	# sigma_p1p2 = np.mean(p1p2, axis=0) - u_p1p2
-	# del p1p2
-	# del points1
-	# del points2
-
-	# A = sigma_p1p2 - sigma_p1p2.T
-	# # print(A.shape)
-
-	# delta = np.array([[A[1,2]],[A[2,0]],[A[0,1]]],dtype=A.dtype)
-
-	# Q_sigma_p1p2 = np.zeros((4,4),dtype=A.dtype)
-
-	# Q_sigma_p1p2[0,0] = np.trace(sigma_p1p2)
-	# Q_sigma_p1p2[1:,0] = delta[:,0]
-	# Q_sigma_p1p2[0,1:] = delta.T[0,:]
-	# temp = sigma_p1p2 + sigma_p1p2.T - (np.trace(sigma_p1p2)*np.eye(3))
-	# Q_sigma_p1p2[1:,1:] = temp[:,:]
-
-	# u,s,v = np.linalg.svd(Q_sigma_p1p2)
-	# # print(u.shape)
-	# # print(s.shape)
-	# # print(v.shape)
-	# # print(np.argmax(s))
-	# ind = np.argmax(s)
-
-	# q_r = v[ind,:]
-	# q_r = q_r.reshape((q_r.shape[0],1))
-	# # print(np.sum(np.square(q_r)))
-	# # print(q_r.shape)
-
-	# R = createR(q_r, np.zeros((3,1)))
-
-	# # sq_q_r = np.square(q_r)
-	# # R[0,0] = sq_q_r[0] + sq_q_r[1] - sq_q_r[2] - sq_q_r[3]
-	# # R[1,1] = sq_q_r[0] + sq_q_r[2] - sq_q_r[1] - sq_q_r[3]
-	# # R[2,2] = sq_q_r[0] + sq_q_r[3] - sq_q_r[1] - sq_q_r[2]
-
-	# # R[0,1] = 2*(q_r[1]*q_r[2] + q_r[0]*q_r[3])
-	# # R[1,0] = 2*(q_r[1]*q_r[2] - q_r[0]*q_r[3])
-
-	# # R[0,2] = 2*(q_r[1]*q_r[3] - q_r[0]*q_r[2])
-	# # R[0,2] = 2*(q_r[1]*q_r[3] + q_r[0]*q_r[2])
-
-	# # R[1,2] = 2*(q_r[2]*q_r[3] - q_r[0]*q_r[1])
-	# # R[2,1] = 2*(q_r[2]*q_r[3] + q_r[0]*q_r[1])
-
-	# q_t = u_p2 - R[:3,:3].dot(u_p1)
-
-	# # q = np.hstack((R,q_t))
-	# # print(R)
-	# # print(q_t)
-	# # q_t[0,0] += 0.0000017
-	# # print(q)
-	# R[0,3] = q_t[0,0]
-	# R[1,3] = q_t[1,0]
-	# R[2,3] = q_t[2,0]
-	# rotated_p1 = R.dot(p[:,:3].T)
-	# rotated_p1 = p[:,:3].T
-	# print(rotated_p1.T[:5])
-	# final_p1 = rotated_p1 + q_t*np.ones(rotated_p1.shape)
-	# final_p1 = X.dot(p.T)
-	# # print(q_t*np.ones(rotated_p1.shape)[:,:5])
-	# # print(final_p1.shape)
-	# final_p1 = final_p1.T
-
-	# ms = np.square(distance(x[:,:3],final_p1[:,:3]))
-	# ms = np.mean(ms, axis = 0)
+	
 	return R, R[:3,:3], R[:3,3] 
 
 def closest_point(p1, tree, p2):
@@ -165,7 +94,7 @@ def ICP(p1, p2):
 		ms = distance(Y[:,:3], p0[:,:3])
 		ms = np.mean(ms,axis=0)
 		diff = m_err - ms
-		if abs(diff) < 1e-10:
+		if 0 < diff < 1e-10:
 			print('Previous error: {}\nCurrent Error: {}\nDifference: {}'.format(m_err, ms, diff))
 			break
 		m_err = ms
@@ -179,6 +108,37 @@ def ICP(p1, p2):
 	# print('Mean square error at iteration {}: {}'.format(0, m_err))
 	return m_err, X0, R0, t, p0
 
+def get_degree_to_meter(ref, points):
+	pi = np.pi
+	arcsin = lambda x: np.arcsin(x)
+	sin = lambda x: np.sin(x)
+	cos = lambda x: np.cos(x)
+	ER = 6371000
+
+	p = points*(pi/180.)
+	ab = ref - p
+	PD = 2*ER * arcsin(np.sqrt( sin(ab[:,0]/2.)**2 + cos(0)*cos(p[:,0])*(sin(ab[:,1]/2.))**2) )
+	return PD
+
+def get_meter_to_degree(ref, points, slat, slng):
+	ER = 6371000.
+	pi = np.pi
+	cos = lambda x: np.cos(x)
+	arcsin = lambda x: np.arcsin(x)
+	lat = (ref[0] - points[:,0]/ER)*(180./pi)
+	lng = (ref[1] - 2*arcsin(np.sqrt(np.square(points[:,1]/(2*ER))/cos(ref[0]))))*(180./pi)
+	sign = 1
+	if np.min(lat) < 0:
+		sign *= -1
+	if sign != slat:
+		lat = lat * -1
+	sign = 1
+	if np.min(lng) < 0:
+		sign *= -1
+	if sign != slng:
+		lng = lng * -1
+	return np.hstack((lat.reshape((lat.shape[0],1)), lng.reshape((lng.shape[0],1))))
+
 if __name__ == '__main__':
 	dir = 'point_cloud_registration'
 	filenames = ['pointcloud1.fuse', 'pointcloud2.fuse']
@@ -188,6 +148,10 @@ if __name__ == '__main__':
 	
 	print(pointcloud1.shape)
 	print(pointcloud2.shape)
+	i1 = pointcloud1[:,3]
+	i2 = pointcloud2[:,3]
+	# print(np.max(i1))
+	# print(i1.shape)
 
 	# ind = np.where(np.equal(pointcloud1[:,3],pointcloud2[:,3]))[0]
 
@@ -196,61 +160,98 @@ if __name__ == '__main__':
 	Increasing the Lat Long values by a scale of 1000 too increase the mean square error
 
 	'''
-	minLat = min(np.min(pointcloud1[:,0]),np.min(pointcloud2[:,0]))
-	minLong = min(np.min(pointcloud1[:,1]), np.min(pointcloud2[:,1]))
-	pointcloud1 = np.hstack((pointcloud1[:,:3], np.ones((pointcloud1.shape[0],1),dtype=pointcloud1.dtype)))
-	pointcloud2 = np.hstack((pointcloud2[:,:3], np.ones((pointcloud2.shape[0],1),dtype=pointcloud2.dtype)))
-	# print(pointcloud1.shape)
-	# exit()
-	scaleLat = 0
-	diffLat = np.max(pointcloud1[:,0]) - minLat
-	while int(diffLat%10) == 0:
-		diffLat *= 10
-		scaleLat += 1
-	scaleLat += 1
-	# print(diffLat, diffLat%10)
-	scaleLong = 0
-	diffLong = np.max(pointcloud1[:,1]) - minLong
-	while int(diffLong%10) == 0:
-		diffLong *= 10
-		scaleLong += 1
-	scaleLong += 1
+	# minLat = min(np.min(pointcloud1[:,0]),np.min(pointcloud2[:,0]))
+	# minLong = min(np.min(pointcloud1[:,1]), np.min(pointcloud2[:,1]))
+	# pointcloud1 = np.hstack((pointcloud1[:,:3], np.ones((pointcloud1.shape[0],1),dtype=pointcloud1.dtype)))
+	# pointcloud2 = np.hstack((pointcloud2[:,:3], np.ones((pointcloud2.shape[0],1),dtype=pointcloud2.dtype)))
+	# # print(pointcloud1.shape)
+	# # exit()
+	# scaleLat = 0
+	# diffLat = np.max(pointcloud1[:,0]) - minLat
+	# while int(diffLat%10) == 0:
+	# 	diffLat *= 10
+	# 	scaleLat += 1
+	# scaleLat += 1
+	# # print(diffLat, diffLat%10)
+	# scaleLong = 0
+	# diffLong = np.max(pointcloud1[:,1]) - minLong
+	# while int(diffLong%10) == 0:
+	# 	diffLong *= 10
+	# 	scaleLong += 1
+	# scaleLong += 1
+
+	signLat = 1
+	if np.min(pointcloud1[:,0]) < 0:
+		signLat *= -1
+	signLong = 1
+	if np.min(pointcloud1[:,1]) < 0:
+		signLong *= -1
+
+	pc1 = np.zeros(pointcloud1.shape, dtype=pointcloud1.dtype)
+	pc2 = np.zeros(pointcloud2.shape, dtype=pointcloud2.dtype)
+
+	pc1[:,0] = get_degree_to_meter(np.array([0.,0.]), np.hstack((pointcloud1[:,0].reshape((pc1.shape[0],1)), np.zeros((pc1.shape[0],1), dtype=np.float32))))[:]
+	pc1[:,1] = get_degree_to_meter(np.array([0.,0.]), np.hstack((np.zeros((pc1.shape[0],1), dtype=np.float32), pointcloud1[:,1].reshape((pc1.shape[0],1)))))[:]
+	pc1[:,2] = pointcloud1[:,2]
+	pc1[:,3] = 1.
+
+	pc2[:,0] = get_degree_to_meter(np.array([0.,0.]), np.hstack((pointcloud2[:,0].reshape((pc2.shape[0],1)), np.zeros((pc2.shape[0],1), dtype=np.float32))))[:]
+	pc2[:,1] = get_degree_to_meter(np.array([0.,0.]), np.hstack((np.zeros((pc2.shape[0],1), dtype=np.float32), pointcloud2[:,1].reshape((pc2.shape[0],1)))))[:]
+	pc2[:,2] = pointcloud2[:,2]
+	pc2[:,3] = 1.
+
+	minX = min(np.min(pc1[:,0]), np.min(pc2[:,0]))
+	minY = min(np.min(pc1[:,1]), np.min(pc2[:,1]))
+
+	# print(minX, minY)
 
 	cs_Mat = np.eye(4,dtype=pointcloud1.dtype)
-	cs_Mat[0,0] *= 10**scaleLat
-	cs_Mat[1,1] *= 10**scaleLong
-	cs_Mat[0,3] = -minLat*(10**scaleLat)
-	cs_Mat[1,3] = -minLong*(10**scaleLong)
+	# cs_Mat[0,0] *= 10**scaleLat
+	# cs_Mat[1,1] *= 10**scaleLong
+	cs_Mat[0,3] = -minX
+	cs_Mat[1,3] = -minY
 	rev_cs_Mat = np.linalg.inv(cs_Mat)
-	print('Coordinate system conversion matrix:')
-	print(cs_Mat)
-	print('Matrix to get back original coordinates:')
-	print(rev_cs_Mat)
-	# pointcloud1[:,0] -= minLat
-	# pointcloud2[:,0] -= minLat
-	# pointcloud1[:,1] -= minLong
-	# pointcloud2[:,1] -= minLong
-	# pointcloud1[:,:2] = pointcloud1[:,:2]*100000
-	# pointcloud2[:,:2] = pointcloud2[:,:2]*100000
-	# print(pointcloud1[:2,:])
+	# print('Coordinate system conversion matrix:')
+	# print(cs_Mat)
+	# print('Matrix to get back original coordinates:')
+	# print(rev_cs_Mat)
+	# # pointcloud1[:,0] -= minLat
+	# # pointcloud2[:,0] -= minLat
+	# # pointcloud1[:,1] -= minLong
+	# # pointcloud2[:,1] -= minLong
+	# # pointcloud1[:,:2] = pointcloud1[:,:2]*100000
+	# # pointcloud2[:,:2] = pointcloud2[:,:2]*100000
+	# # print(pointcloud1[:2,:])
+	# print(pointcloud2[:2,:2])
+	# dist = np.zeros((2,2))
+	# dist[:,0] = get_degree_to_meter(np.array([0.,0.]), np.hstack((pointcloud2[:2,0].reshape((2,1)), np.zeros((2,1)))))
+	# dist[:,1] = get_degree_to_meter(np.array([0.,0.]), np.hstack((np.zeros((2,1)), pointcloud2[:2,1].reshape((2,1)))))
+	# print(dist)
 
-	pc1 = cs_Mat.dot(pointcloud1.T)
+	# p = get_meter_to_degree(np.array([0.,0.]), dist, signLat, signLong)
+	# print(p)
+
+	pc1 = cs_Mat.dot(pc1.T)
 	pc1 = pc1.T
-	pc2 = cs_Mat.dot(pointcloud2.T)
+	pc2 = cs_Mat.dot(pc2.T)
 	pc2 = pc2.T
 
-	# with open('p2_1.xyz','w') as file:
-	# 	for i in range(temp.shape[0]):
-	# 		file.write('{} {} {}\n'.format(temp[i,0],temp[i,1],temp[i,2]))
-
-	# temp = rev_cs_Mat.dot(pointcloud1.T)
+	# temp = rev_cs_Mat.dot(pc2.T)
 	# temp = temp.T
 
-	# with open('p1_1.xyz','w') as file:
+	# with open('p2_1.pts','w') as file:
 	# 	for i in range(temp.shape[0]):
-	# 		file.write('{} {} {}\n'.format(temp[i,0],temp[i,1],temp[i,2]))
+	# 		file.write('{},{},{},{}\n'.format(temp[i,0],temp[i,1],temp[i,2],i2[i]))
+
+	# temp = rev_cs_Mat.dot(pc1.T)
+	# temp = temp.T
+
+	# with open('p1_1.pts','w') as file:
+	# 	for i in range(temp.shape[0]):
+	# 		file.write('{},{},{},{}\n'.format(temp[i,0],temp[i,1],temp[i,2],i1[i]))
 
 	# del temp
+	# exit()
 
 	ms = None
 	print()
@@ -266,27 +267,33 @@ if __name__ == '__main__':
 	print('Translation Matrix:')
 	print(t)
 
-	print('\nComplete Transformation Matrix:')
-	mat = rev_cs_Mat.dot(X.dot(cs_Mat))
-	print(mat)
+	# print('\nComplete Transformation Matrix:')
+	# mat = rev_cs_Mat.dot(X.dot(cs_Mat))
+	# print(mat)
 	# print(pointcloud2[:5])
 	# print(final_p1[:5])
+	# pc1 = rev_cs_Mat.dot(pc1.T)
+	# pc1 = pc1.T
+	# pc1[:,:2] = get_meter_to_degree(np.array([0.,0.]), pc1[:,:2], signLat, signLong)[:,:]
+	# pc2 = rev_cs_Mat.dot(pc2.T)
+	# pc2 = pc2.T
+	# pc2[:,:2] = get_meter_to_degree(np.array([0.,0.]), pc2[:,:2], signLat, signLong)[:,:]
 
 	fig = plt.figure()
 	ax = fig.add_subplot(111, projection='3d')
 
 	npoints = 10000
 
-	X = pointcloud1[:npoints,0]
-	Y = pointcloud1[:npoints,1]
-	Z = pointcloud1[:npoints,2]
+	X = pc1[:npoints,0]
+	Y = pc1[:npoints,1]
+	Z = pc1[:npoints,2]
 
-	ax.scatter(X,Y,Z, color='b', marker='o', label='Point Cloud 1')
+	# ax.scatter(X,Y,Z, color='b', marker='o', label='Point Cloud 1')
 	# ax.plot_wireframe(X,Y,Z, color='b')
 
-	X = pointcloud2[:npoints,0]
-	Y = pointcloud2[:npoints,1]
-	Z = pointcloud2[:npoints,2]
+	X = pc2[:npoints,0]
+	Y = pc2[:npoints,1]
+	Z = pc2[:npoints,2]
 
 	# ax.plot_wireframe(X,Y,Z, color='r')
 	ax.scatter(X,Y,Z, color='r', marker='o',label='Point Cloud 2')
@@ -296,11 +303,12 @@ if __name__ == '__main__':
 	ax.set_zlabel('Altitude')
 	
 	# final_p1 = mat.dot(pointcloud1.T)
-	final_p1 = rev_cs_Mat.dot(final_p1.T)
-	final_p1 = final_p1.T
-	# with open('p1_fin_perfect.xyz','w') as file:
+	# final_p1 = rev_cs_Mat.dot(final_p1.T)
+	# final_p1 = final_p1.T
+	# final_p1[:,:2] = get_meter_to_degree(np.array([0.,0.]), final_p1[:,:2], signLat, signLong)[:,:]
+	# with open('p1_fin.pts','w') as file:
 	# 	for i in range(final_p1.shape[0]):
-	# 		file.write('{} {} {}\n'.format(final_p1[i,0],final_p1[i,1],final_p1[i,2]))
+	# 		file.write('{},{},{},{}\n'.format(final_p1[i,0],final_p1[i,1],final_p1[i,2],i1[i]))
 
 	X = final_p1[:npoints,0]
 	Y = final_p1[:npoints,1]
